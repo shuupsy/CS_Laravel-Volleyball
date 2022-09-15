bd5fabbf57670414f82d91e700c0110bcc43470e
shuupsy <sylvaine.ruan@gmail.com>
2022-09-15 12:21:55 +0200

    Header + Footer + Creation Controllers

---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-white dark:bg-gray-900">
+    <div class="container p-6 mx-auto">
+        <hr class="h-px my-6 bg-gray-200 border-none dark:bg-gray-700">
+
+        <div>
+            <p class="text-center text-gray-500 dark:text-gray-400">WE ARE CHAMPIONS</p>
+        </div>
+    </div>
+</footer>
