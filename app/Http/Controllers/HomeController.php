78e249b2778080989d441a112efa2b9a1724cb1f
shuupsy <sylvaine.ruan@gmail.com>
2022-09-17 22:52:17 +0200

    HomeController: filtre

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Equipe;
+use App\Models\Joueur;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        $all_equipes = Equipe::all();
+        $full_team = Equipe::withCount('joueurs')
+            ->has('joueurs', 6)
+            ->get();
 
-        return view('pages.home', compact('all_equipes'));
+        $nonfull_team = Equipe::withCount('joueurs')
+            ->has('joueurs', '<', 6)
+            ->get();
+
+        $noteam_player = Joueur::where('equipe_id', null)
+            ->take(4)
+            ->get();
+
+        $withteam_player = Joueur::whereNot('equipe_id', null)
+            ->take(4)
+            ->get();
+
+        $europe_team = Equipe::where('continent_id', 1)->get();
+        $outside_team = Equipe::whereNot('continent_id', 1)->get();
+
+        $withteam_female = Joueur::whereNot('equipe_id', null)
+            ->where('genre', 'F')
+            ->get();
+
+        $withteam_male = Joueur::whereNot('equipe_id', null)
+            ->whereNot('genre', 'F')
+            ->get();
+
+
+        return view('pages.home', compact('full_team', 'nonfull_team', 'noteam_player', 'withteam_player', 'europe_team', 'outside_team', 'withteam_female', 'withteam_male'));
 
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,34 +3,74 @@
 @section('content')
     <section>
         <h1 class='text-4xl'>Équipes remplies</h1>
+        @foreach ($full_team as $team)
+        <ul>
+            <li>{{ $team -> nom_equipe }}</li>
+        </ul>
+    @endforeach
 
     </section>
 
     <section>
         <h1 class='text-4xl'>Équipes NON remplies</h1>
+        @foreach ($nonfull_team as $team)
+            <ul>
+                <li>{{ $team -> nom_equipe }}</li>
+            </ul>
+        @endforeach
     </section>
 
     <section>
         <h1 class='text-4xl'>Joueurs SANS équipe</h1>
+        @foreach ($noteam_player as $player)
+            <ul>
+                <li>{{ $player -> nom_joueur }}</li>
+            </ul>
+        @endforeach
     </section>
 
     <section>
         <h1 class='text-4xl'>Joueurs AVEC équipe</h1>
+        @foreach ($withteam_player as $player)
+            <ul>
+                <li>{{ $player -> nom_joueur }}</li>
+            </ul>
+        @endforeach
     </section>
 
     <section>
         <h1 class='text-4xl'>Équipes d'Europe</h1>
+        @foreach ($europe_team as $team)
+            <ul>
+                <li>{{ $team -> nom_equipe }}</li>
+            </ul>
+        @endforeach
     </section>
 
     <section>
         <h1 class='text-4xl'>Équipes HORS Europe</h1>
+        @foreach ($outside_team as $team)
+            <ul>
+                <li>{{ $team -> nom_equipe }}</li>
+            </ul>
+        @endforeach
     </section>
 
     <section>
         <h1 class='text-4xl'>Joueuses, avec équipe</h1>
+        @foreach ($withteam_female as $player)
+            <ul>
+                <li>{{ $player -> nom_joueur }}</li>
+            </ul>
+        @endforeach
     </section>
 
     <section>
         <h1 class='text-4xl'>Joueurs, avec équipe</h1>
+        @foreach ($withteam_male as $player)
+            <ul>
+                <li>{{ $player -> nom_joueur }}</li>
+            </ul>
+        @endforeach
     </section>
 @endsection
